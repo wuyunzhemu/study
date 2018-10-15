b3bf3e155695b5f28a2d658311d3986fad4ad6b5
wuyunzhemu <415159489@qq.com>
2018-10-15 17:37:08 +0800

    typescript练习

---
+++ typescript/readme.md
@@ -0,0 +1,3 @@
+typescipt 是js 超集
+给js提供了类型系统,让js适合大型项目开发。
+写ts编译成js
---
+++ typescript/tys.ts
@@ -0,0 +1,6 @@
+function greeter(person:string):string{
+  return 123;
+  return "Hello,"+person;
+}
+var user:string= 123;
+document.body.innerHTML = greeter(user);
