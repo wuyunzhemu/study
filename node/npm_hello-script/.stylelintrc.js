26d483ce1a73080b2fbeb5ad8e3523c345d763f1
wuyunzhemu <415159489@qq.com>
2018-10-25 23:29:30 +0800

    新增eslint和stylelint练习项目

---
+++ node/npm_hello-script/.stylelintrc.js
@@ -0,0 +1,10 @@
+// common.js 模块化方案
+// export default import from es6的模块化方案
+module.exports={
+  "extends":"stylelint-config-standard",
+  "rules":{
+    "at-rule-empty-line-before":null
+  }
+}
+// es6 的模块化方案
+// export default{}
