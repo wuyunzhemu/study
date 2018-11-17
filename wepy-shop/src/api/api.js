e6bd4c532dc50f0da6ae850d5b86042b3c487819
wuyunzhemu <415159489@qq.com>
2018-11-17 16:08:51 +0800

    新增一个wepy练习

---
+++ wepy-shop/src/api/api.js
@@ -0,0 +1,10 @@
+// Application interface
+// Server web Application
+import{
+  wxRequest
+} from "@/utils/wxRequest"
+const apiMall = 'https://sujiefs.com';
+const getAdList = (params) => wxRequest(params,apiMall+'/api/adverts/list')
+export default{
+  getAdList
+}
