7ae5fb53f50b24a25c19b6935127fef39fc36e8f
wuyunzhemu <415159489@qq.com>
2018-11-29 18:20:18 +0800

    react练习

---
+++ react/jsx/readme.md
@@ -0,0 +1,23 @@
+JSX React 模板的语法
+Component render()
+函数式组件 return()
+template 跟VUE不一样，在JS里 JSX babel去做编译模板
+
+1. JSX是一切组件的基础，函数式组件，以返回JSX为目标
+2. JSX 真的就是JS，可以执行一切JS
+3. React JSX React.createElement() 更优雅地表达
+可读性，
+JSX用来描述UI信息，React.createElement()更能表达这点
+
+不管是小程序 vue react 都是一模一样的 都是MVVM
+模板 数据绑定的{} {{}}
+JSX 不支持wx:for 还好它是纯JS的，可以自己写逻辑 灵活，
+(
+  <ul>
+    {
+      users.map((user,index)=>
+        <li key = {index}>{user.name}</li>
+      )
+    }
+  </ul>
+)
---
+++ react/try-all-react/src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+class HelloUser extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      username:'tylermcginnis'
+    }
+    // setTimeout(()=>{
+    //   this.setState({
+    //     username:'嘉欣'
+    //   })
+    // },2000)
+  }
+  render(){
+    this.handleChange = this.handleChange.bind(this);
+    return (
+      <div>
+        Hello  {this.state.username}
+        <input 
+          type = "text"
+          value = {this.state.username}
+          onChange = {(e)=>{this.handleChange(e)}}
+        />
+      </div>
+
+      // <div>Hello World!</div>
+      // React.createElement('div',null,React.createElement('p',null,'axiba'))
+    )
+  }
+  handleChange(e){
+    this.setState({
+      username : e.target.value
+    })
+  }
+}
+
+ReactDOM.render(<HelloUser />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
