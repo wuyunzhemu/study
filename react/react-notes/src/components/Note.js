cada159927f6fd52f105055c0b4fdd76e238a3b5
wuyunzhemu <415159489@qq.com>
2018-12-03 21:33:42 +0800

    react 记事本demo练习

---
+++ react/react-notes/src/components/Note.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-CN');
+
+// Note? 展示单条笔记，状态
+class Note extends Component {
+  constructor(props){
+    super(props);
+  }
+  state = {
+    entity:this.props.entity,
+    body:this.props.entity.body,
+    update:this.props.entity.meta.update || this.props.entity.meta.created,
+    open:false,
+
+  }
+  render(){
+    const ts = moment(this.state.update).fromNow()
+    return(
+      <div className="item">
+        {this.state.body}{ts}
+      </div>
+    );
+  }
+}
+export default Note;
