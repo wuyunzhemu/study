a5c6591033d0472b07e3e70d9988d0e5eaf262db
wuyunzhemu <415159489@qq.com>
2018-12-27 21:01:13 +0800

    react列表

--- react/mango-music-chapter1 2/src/App.js
+++ react/mango-music-chapter1 2/src/App.js
@@ -1,47 +1,41 @@
 import React, { Component } from 'react';
-import Recommend from './recommend/Recommend';
-import Ranking from './ranking/Ranking';
-import Search from './search/Search';
-import logo from './assets/imgs/logo.png';
-import './assets/stylus/reset.styl';
+import {BrowserRouter as Router,Route,Switch,NavLink,Redirect} from 'react-router-dom'
 import './App.styl';
-import {BrowserRouter as Router,Route,Switch,Redirect,NavLink } from 'react-router-dom';
+import logo from './assets/imgs/logo.png'
+import Recommend from './recommend/Recommend'
+import Ranking from './ranking/Ranking'
+import Search from './search/Search'
+import "./assets/stylus/reset.styl"
 
 class App extends Component {
   render() {
     return (
-      <Router>  
-        <div className="app">
+      <Router>
+        <div className="app skin-app">
           <header className="app-header">
-            <img src={logo} alt="logo"/>
+            <img src={logo} className="app-logo" alt="logo"/>
             <h1 className="app-title">React Music</h1>
           </header>
           <div className="music-tab">
             <div className="tab-item">
-              <NavLink to="/recommend" className="nav-link">
-                <span>首页</span>
-              </NavLink>
+              <NavLink to="/recommend" className="nav-link"><span>推荐</span></NavLink>
             </div>
             <div className="tab-item">
-              <NavLink to="/ranking" className="nav-link">
-                <span>排行榜</span>
-              </NavLink>
+              <NavLink to="/ranking" className="nav-link"><span>排行榜</span></NavLink>
             </div>
             <div className="tab-item">
-              <NavLink to="/search" className="nav-link">
-                <span>搜索</span>
-              </NavLink>
-              <div className="music-view">
-                <Switch>
-                  <Route path = "/recommend" component={Recommend}/>
-                  <Route path = "/ranking" component={Ranking}/>
-                  <Route path = "/search" component={Search}/>
-                  <Redirect from="/" to="/recommend"/>
-                </Switch>
-              </div>
+              <NavLink to="/search" className="nav-link"><span>搜索</span></NavLink>
             </div>
           </div>
-        </div>
+          <div className="music-view">
+            <Switch>
+              <Route path="/recommend" component={Recommend}/>
+              <Route path="/ranking" component={Ranking}/>
+              <Route path="/search" component={Search}/>
+              <Redirect from="/" to="/recommend" />
+           </Switch>
+          </div>
+        </div>       
       </Router>
     );
   }
